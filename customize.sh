fe7f682060391e1de722b6f785e83cbb74031fbc
zeshch <82803312+zeshch@users.noreply.github.com>
2021-04-19 16:26:48 +0800

    Modify default IP
    Modify default IP for 10.10.10.1

--- customize.sh
+++ customize.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Author: EarthZeng
+# form eSir AutoBuild script,for team user.
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' openwrt/package/base-files/files/bin/config_generate
